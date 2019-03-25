583241a3a78596cbb9eb0063ba6d5ee364c2f839
Ernest-xu <xjm6528@sina.com>
2019-03-25 10:57:32 +0800

    添加实体类

---
+++ QST Maven Webapp/src/main/java/Ernest/Entity/PostsetbXm.java
@@ -0,0 +1,287 @@
+package Ernest.Entity;
+
+
+/**
+ * PmProjectProjectInformationtablePostsetbXm entity. @author MyEclipse
+ * Persistence Tools
+ */
+
+
+public class PostsetbXm implements java.io.Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	// Fields
+
+	private String fid;
+	private String fno;
+	private String fpostName;
+	private String fpostWriteName;
+	private String fpostWriteId;
+	private String fpostCode;
+	private String fpostId;
+	private String fpostState;
+	private String fpostType;
+	private String fmasterId;
+	private String fprojectName;
+	private String fprojectCode;
+	private String froleName;
+	private String fxmbType;
+	private String fxmbName;
+	private String froleId;
+	private String fmd5;
+	private String fpostXlb;
+	private String fwriteName;
+	private String fwriteId;
+	private String fwriteMd5;
+
+	// Constructors
+
+	/** default constructor */
+	public PostsetbXm() {
+	}
+
+	/** minimal constructor */
+	public PostsetbXm(String fid) {
+		this.fid = fid;
+	}
+
+	/** full constructor */
+	public PostsetbXm(String fid, String fno, String fpostName, String fpostWriteName,
+			String fpostWriteId, String fpostCode, String fpostId, String fpostState, String fpostType,
+			String fmasterId, String fprojectName, String fprojectCode, String froleName, String fxmbType,
+			String fxmbName, String froleId, String fmd5, String fpostXlb, String fwriteName, String fwriteId,
+			String fwriteMd5) {
+		this.fid = fid;
+		this.fno = fno;
+		this.fpostName = fpostName;
+		this.fpostWriteName = fpostWriteName;
+		this.fpostWriteId = fpostWriteId;
+		this.fpostCode = fpostCode;
+		this.fpostId = fpostId;
+		this.fpostState = fpostState;
+		this.fpostType = fpostType;
+		this.fmasterId = fmasterId;
+		this.fprojectName = fprojectName;
+		this.fprojectCode = fprojectCode;
+		this.froleName = froleName;
+		this.fxmbType = fxmbType;
+		this.fxmbName = fxmbName;
+		this.froleId = froleId;
+		this.fmd5 = fmd5;
+		this.fpostXlb = fpostXlb;
+		this.fwriteName = fwriteName;
+		this.fwriteId = fwriteId;
+		this.fwriteMd5 = fwriteMd5;
+	}
+
+	// Property accessors
+
+	public String getFid() {
+		return this.fid;
+	}
+
+	public void setFid(String fid) {
+		this.fid = fid;
+	}
+
+
+	public String getFno() {
+		return this.fno;
+	}
+
+	public void setFno(String fno) {
+		this.fno = fno;
+	}
+
+
+	public String getFpostName() {
+		return this.fpostName;
+	}
+
+	public void setFpostName(String fpostName) {
+		this.fpostName = fpostName;
+	}
+
+
+	public String getFpostWriteName() {
+		return this.fpostWriteName;
+	}
+
+	public void setFpostWriteName(String fpostWriteName) {
+		this.fpostWriteName = fpostWriteName;
+	}
+
+
+	public String getFpostWriteId() {
+		return this.fpostWriteId;
+	}
+
+	public void setFpostWriteId(String fpostWriteId) {
+		this.fpostWriteId = fpostWriteId;
+	}
+
+
+	public String getFpostCode() {
+		return this.fpostCode;
+	}
+
+	public void setFpostCode(String fpostCode) {
+		this.fpostCode = fpostCode;
+	}
+
+
+	public String getFpostId() {
+		return this.fpostId;
+	}
+
+	public void setFpostId(String fpostId) {
+		this.fpostId = fpostId;
+	}
+
+
+	public String getFpostState() {
+		return this.fpostState;
+	}
+
+	public void setFpostState(String fpostState) {
+		this.fpostState = fpostState;
+	}
+
+
+	public String getFpostType() {
+		return this.fpostType;
+	}
+
+	public void setFpostType(String fpostType) {
+		this.fpostType = fpostType;
+	}
+
+
+	public String getFmasterId() {
+		return this.fmasterId;
+	}
+
+	public void setFmasterId(String fmasterId) {
+		this.fmasterId = fmasterId;
+	}
+
+
+	public String getFprojectName() {
+		return this.fprojectName;
+	}
+
+	public void setFprojectName(String fprojectName) {
+		this.fprojectName = fprojectName;
+	}
+
+
+	public String getFprojectCode() {
+		return this.fprojectCode;
+	}
+
+	public void setFprojectCode(String fprojectCode) {
+		this.fprojectCode = fprojectCode;
+	}
+
+
+	public String getFroleName() {
+		return this.froleName;
+	}
+
+	public void setFroleName(String froleName) {
+		this.froleName = froleName;
+	}
+
+
+	public String getFxmbType() {
+		return this.fxmbType;
+	}
+
+	public void setFxmbType(String fxmbType) {
+		this.fxmbType = fxmbType;
+	}
+
+
+	public String getFxmbName() {
+		return this.fxmbName;
+	}
+
+	public void setFxmbName(String fxmbName) {
+		this.fxmbName = fxmbName;
+	}
+
+
+	public String getFroleId() {
+		return this.froleId;
+	}
+
+	public void setFroleId(String froleId) {
+		this.froleId = froleId;
+	}
+
+
+	public String getFmd5() {
+		return this.fmd5;
+	}
+
+	public void setFmd5(String fmd5) {
+		this.fmd5 = fmd5;
+	}
+
+
+	public String getFpostXlb() {
+		return this.fpostXlb;
+	}
+
+	public void setFpostXlb(String fpostXlb) {
+		this.fpostXlb = fpostXlb;
+	}
+
+
+	public String getFwriteName() {
+		return this.fwriteName;
+	}
+
+	public void setFwriteName(String fwriteName) {
+		this.fwriteName = fwriteName;
+	}
+
+
+	public String getFwriteId() {
+		return this.fwriteId;
+	}
+
+	public void setFwriteId(String fwriteId) {
+		this.fwriteId = fwriteId;
+	}
+
+
+	public String getFwriteMd5() {
+		return this.fwriteMd5;
+	}
+
+	public void setFwriteMd5(String fwriteMd5) {
+		this.fwriteMd5 = fwriteMd5;
+	}
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#toString()
+	 */
+	@Override
+	public String toString() {
+		return "PostsetbXm [fid=" + fid + ", fno=" + fno + ", fpostName=" + fpostName + ", fpostWriteName="
+				+ fpostWriteName + ", fpostWriteId=" + fpostWriteId + ", fpostCode=" + fpostCode + ", fpostId="
+				+ fpostId + ", fpostState=" + fpostState + ", fpostType=" + fpostType + ", fmasterId=" + fmasterId
+				+ ", fprojectName=" + fprojectName + ", fprojectCode=" + fprojectCode + ", froleName=" + froleName
+				+ ", fxmbType=" + fxmbType + ", fxmbName=" + fxmbName + ", froleId=" + froleId + ", fmd5=" + fmd5
+				+ ", fpostXlb=" + fpostXlb + ", fwriteName=" + fwriteName + ", fwriteId=" + fwriteId + ", fwriteMd5="
+				+ fwriteMd5 + "]";
+	}
+	
+	
+	
+
+}
---
+++ QST Maven Webapp/src/main/java/Ernest/Entity/PostsetbXm3gw.java
@@ -0,0 +1,313 @@
+package Ernest.Entity;
+
+
+
+
+/**
+ * PmProjectProjectInformationtablePostsetbXm3gw entity. @author MyEclipse
+ * Persistence Tools
+ */
+
+
+public class PostsetbXm3gw implements java.io.Serializable {
+
+	// Fields
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private String fid;
+	private String fno;
+	private String fpostName;
+	private String fpostWriteName;
+	private String fpostWriteId;
+	private String fpostCode;
+	private String fpostId;
+	private String fpostState;
+	private String fpostType;
+	private String fmasterId;
+	private String fprojectName;
+	private String fprojectCode;
+	private String froleName;
+	private String froleId;
+	private String fmd5;
+	private String fpostXlb;
+	private String fteamT;
+	private String fteamleibie;
+	private String fpostxmid;
+	private String fpostxmid2;
+	private String fpostxmid1;
+	private String fysxs1;
+	private PostsetbXm postsetbXm;
+	// Constructors
+
+	/** default constructor */
+	public PostsetbXm3gw() {
+	}
+
+	/** minimal constructor */
+	public PostsetbXm3gw(String fid) {
+		this.fid = fid;
+	}
+
+	/** full constructor */
+	public PostsetbXm3gw(String fid, String fno, String fpostName,
+			String fpostWriteName, String fpostWriteId, String fpostCode, String fpostId, String fpostState,
+			String fpostType, String fmasterId, String fprojectName, String fprojectCode, String froleName,
+			String froleId, String fmd5, String fpostXlb, String fteamT, String fteamleibie, String fpostxmid,
+			String fpostxmid2, String fpostxmid1, String fysxs1) {
+		this.fid = fid;
+		this.fno = fno;
+		this.fpostName = fpostName;
+		this.fpostWriteName = fpostWriteName;
+		this.fpostWriteId = fpostWriteId;
+		this.fpostCode = fpostCode;
+		this.fpostId = fpostId;
+		this.fpostState = fpostState;
+		this.fpostType = fpostType;
+		this.fmasterId = fmasterId;
+		this.fprojectName = fprojectName;
+		this.fprojectCode = fprojectCode;
+		this.froleName = froleName;
+		this.froleId = froleId;
+		this.fmd5 = fmd5;
+		this.fpostXlb = fpostXlb;
+		this.fteamT = fteamT;
+		this.fteamleibie = fteamleibie;
+		this.fpostxmid = fpostxmid;
+		this.fpostxmid2 = fpostxmid2;
+		this.fpostxmid1 = fpostxmid1;
+		this.fysxs1 = fysxs1;
+	}
+
+	
+
+	/**
+	 * @return the postsetbXm
+	 */
+	public PostsetbXm getPostsetbXm() {
+		return postsetbXm;
+	}
+
+	/**
+	 * @param postsetbXm the postsetbXm to set
+	 */
+	public void setPostsetbXm(PostsetbXm postsetbXm) {
+		this.postsetbXm = postsetbXm;
+	}
+
+	public String getFid() {
+		return this.fid;
+	}
+
+	public void setFid(String fid) {
+		this.fid = fid;
+	}
+
+
+	public String getFno() {
+		return this.fno;
+	}
+
+	public void setFno(String fno) {
+		this.fno = fno;
+	}
+
+
+	public String getFpostName() {
+		return this.fpostName;
+	}
+
+	public void setFpostName(String fpostName) {
+		this.fpostName = fpostName;
+	}
+
+
+	public String getFpostWriteName() {
+		return this.fpostWriteName;
+	}
+
+	public void setFpostWriteName(String fpostWriteName) {
+		this.fpostWriteName = fpostWriteName;
+	}
+
+
+	public String getFpostWriteId() {
+		return this.fpostWriteId;
+	}
+
+	public void setFpostWriteId(String fpostWriteId) {
+		this.fpostWriteId = fpostWriteId;
+	}
+
+
+	public String getFpostCode() {
+		return this.fpostCode;
+	}
+
+	public void setFpostCode(String fpostCode) {
+		this.fpostCode = fpostCode;
+	}
+
+
+	public String getFpostId() {
+		return this.fpostId;
+	}
+
+	public void setFpostId(String fpostId) {
+		this.fpostId = fpostId;
+	}
+
+
+	public String getFpostState() {
+		return this.fpostState;
+	}
+
+	public void setFpostState(String fpostState) {
+		this.fpostState = fpostState;
+	}
+
+
+	public String getFpostType() {
+		return this.fpostType;
+	}
+
+	public void setFpostType(String fpostType) {
+		this.fpostType = fpostType;
+	}
+
+
+	public String getFmasterId() {
+		return this.fmasterId;
+	}
+
+	public void setFmasterId(String fmasterId) {
+		this.fmasterId = fmasterId;
+	}
+
+
+	public String getFprojectName() {
+		return this.fprojectName;
+	}
+
+	public void setFprojectName(String fprojectName) {
+		this.fprojectName = fprojectName;
+	}
+
+
+	public String getFprojectCode() {
+		return this.fprojectCode;
+	}
+
+	public void setFprojectCode(String fprojectCode) {
+		this.fprojectCode = fprojectCode;
+	}
+
+
+	public String getFroleName() {
+		return this.froleName;
+	}
+
+	public void setFroleName(String froleName) {
+		this.froleName = froleName;
+	}
+
+
+	public String getFroleId() {
+		return this.froleId;
+	}
+
+	public void setFroleId(String froleId) {
+		this.froleId = froleId;
+	}
+
+
+	public String getFmd5() {
+		return this.fmd5;
+	}
+
+	public void setFmd5(String fmd5) {
+		this.fmd5 = fmd5;
+	}
+
+
+	public String getFpostXlb() {
+		return this.fpostXlb;
+	}
+
+	public void setFpostXlb(String fpostXlb) {
+		this.fpostXlb = fpostXlb;
+	}
+
+
+	public String getFteamT() {
+		return this.fteamT;
+	}
+
+	public void setFteamT(String fteamT) {
+		this.fteamT = fteamT;
+	}
+
+
+	public String getFteamleibie() {
+		return this.fteamleibie;
+	}
+
+	public void setFteamleibie(String fteamleibie) {
+		this.fteamleibie = fteamleibie;
+	}
+
+
+	public String getFpostxmid() {
+		return this.fpostxmid;
+	}
+
+	public void setFpostxmid(String fpostxmid) {
+		this.fpostxmid = fpostxmid;
+	}
+
+
+	public String getFpostxmid2() {
+		return this.fpostxmid2;
+	}
+
+	public void setFpostxmid2(String fpostxmid2) {
+		this.fpostxmid2 = fpostxmid2;
+	}
+
+
+	public String getFpostxmid1() {
+		return this.fpostxmid1;
+	}
+
+	public void setFpostxmid1(String fpostxmid1) {
+		this.fpostxmid1 = fpostxmid1;
+	}
+
+
+	public String getFysxs1() {
+		return this.fysxs1;
+	}
+	
+	public void setFysxs1(String fysxs1) {
+		this.fysxs1 = fysxs1;
+	}
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#toString()
+	 */
+	@Override
+	public String toString() {
+		return "PostsetbXm3gw [fid=" + fid + ", fno=" + fno + ", fpostName=" + fpostName + ", fpostWriteName="
+				+ fpostWriteName + ", fpostWriteId=" + fpostWriteId + ", fpostCode=" + fpostCode + ", fpostId="
+				+ fpostId + ", fpostState=" + fpostState + ", fpostType=" + fpostType + ", fmasterId=" + fmasterId
+				+ ", fprojectName=" + fprojectName + ", fprojectCode=" + fprojectCode + ", froleName=" + froleName
+				+ ", froleId=" + froleId + ", fmd5=" + fmd5 + ", fpostXlb=" + fpostXlb + ", fteamT=" + fteamT
+				+ ", fteamleibie=" + fteamleibie + ", fpostxmid=" + fpostxmid + ", fpostxmid2=" + fpostxmid2
+				+ ", fpostxmid1=" + fpostxmid1 + ", fysxs1=" + fysxs1 + "]";
+	}
+
+	
+
+}
